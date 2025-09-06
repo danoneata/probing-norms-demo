f7ea5707860d41b9d50888ad424e4c7b605adcdb
Dan Oneata <dan.oneata@gmail.com>
2025-09-06 17:31:42 +0300

    Cache resources

--- show_data.py
+++ show_data.py
@@ -16,47 +16,58 @@ def get_image_path_online(image_file):
 
 st.set_page_config(layout="wide")
 
-supercategory_to_concepts = get_supercategories()
-concept_and_supercategory = [
-    (concept, supercategory)
-    for supercategory, concepts in supercategory_to_concepts.items()
-    for concept in concepts
-]
-concept_to_supercategories = multimap(concept_and_supercategory)
+
+@st.cache_resource
+def load_resources():
+    dataset = DATASETS["things"]()
+
+    norms_loader = NORMS_LOADERS["mcrae-x-things"]()
+    attribute_to_concepts, _, _ = norms_loader()
+    concept_and_attribute = [
+        (concept, attribute)
+        for attribute, concepts in attribute_to_concepts.items()
+        for concept in concepts
+    ]
+    concept_to_attributes = multimap(concept_and_attribute)
+
+    supercategory_to_concepts = get_supercategories()
+    concept_and_supercategory = [
+        (concept, supercategory)
+        for supercategory, concepts in supercategory_to_concepts.items()
+        for concept in concepts
+    ]
+    concept_to_supercategories = multimap(concept_and_supercategory)
+
+    return dataset, concept_to_attributes, concept_to_supercategories
+
+
+dataset, concept_to_attributes, concept_to_supercategories = load_resources()
+concepts = sorted(dataset.class_to_label.keys())
+
 
 def get_supercategories_str(concept):
     return ", ".join(concept_to_supercategories.get(concept, []))
 
-dataset = DATASETS["things"]()
-norms_loader = NORMS_LOADERS["mcrae-x-things"]()
-attribute_to_concepts, _, attributes = norms_loader()
-concept_and_attribute = [
-    (concept, attribute)
-    for attribute, concepts in attribute_to_concepts.items()
-    for concept in concepts
-]
-concept_to_attributes = multimap(concept_and_attribute)
-
-concepts = dataset.class_to_label.keys()
-concepts = sorted(concepts)
-
 cols = st.columns([1, 1, 4])
 
-cols[0].markdown("### Concept")
-concept = cols[0].selectbox("Select a concept", concepts)
-label = dataset.class_to_label[concept]
-cols[0].markdown("### Supercategories")
-cols[0].markdown(get_supercategories_str(concept))
+with cols[0]:
+    st.markdown("### Concept")
+    concept = cols[0].selectbox("Select a concept", concepts)
+    label = dataset.class_to_label[concept]
+    st.markdown("### Supercategories")
+    st.markdown(get_supercategories_str(concept))
 
-cols[1].markdown("### Attributes")
-cols[1].markdown("\n".join(f"- {a}" for a in concept_to_attributes.get(concept, [])))
+with cols[1]:
+    st.markdown("### Attributes")
+    st.markdown("\n".join(f"- {a}" for a in concept_to_attributes.get(concept, [])))
 
-cols[2].markdown("### Images")
-idxs, = np.where(np.array(dataset.labels) == label)
+with cols[2]:
+    st.markdown("### Images")
+    idxs, = np.where(np.array(dataset.labels) == label)
 
-num_cols = 6
-for group in partition_all(num_cols, idxs):
-    colss = cols[2].columns(num_cols)
-    for i, idx in enumerate(group):
-        image_file = dataset.image_files[idx]
-        colss[i].image(get_image_path_online(image_file))
+    num_cols = 6
+    for group in partition_all(num_cols, idxs):
+        colss = st.columns(num_cols)
+        for i, idx in enumerate(group):
+            image_file = dataset.image_files[idx]
+            colss[i].image(get_image_path_online(image_file))
